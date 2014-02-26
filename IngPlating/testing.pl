c72eaf5e9ed6fb11361a6839a0ed16f0949778af
Gabs00 <gewen87@gmail.com>
2014-02-26 01:19:40 -0500

    Moved Plate class specific info to Plate class

--- IngPlating/testing.pl
+++ IngPlating/testing.pl
@@ -1,18 +1,11 @@
 #!/usr/bin/perl
 
 use Modern::Perl;
-use LWP::UserAgent;
-use XML::Simple qw(:strict);
 use Data::Dumper;
-use Ingredient;
-use Recipe;
+use Plate;
 
 
-
-
-
-
-my $key = "yas6vg4wwgqdaky4ht2pkrh4";
+my $plate = Plate->new();
 my $query = "salt";
 
 my $recipe = Recipe->new(recipeName => "Cookies", servings => 4, ingredients => {
@@ -24,10 +17,10 @@ my $recipe = Recipe->new(recipeName => "Cookies", servings => 4, ingredients => 
 
 $recipe->price(20);
 
-print "Price \$",$recipe->price,"\nPrice per serving: \$",$recipe->pricePerServing; 
+print "Price \$",$recipe->price,"\nPrice per serving: \$",$recipe->pricePerServing, "\n"; 
 
 =Ingredients Testing
-my $results = getItem($key,$query);
+my $results = $plate->getItem($query);
 
 my @keys = keys %{ $results->{'items'}->{'item'} };
 
@@ -36,7 +29,7 @@ my %first = %{ $results->{'items'}->{'item'}->{$keys[3]} };
 #checks if size is defined, if not, the info is contained in name
 my $containSize = defined($first{'size'}) ? $first{'size'}:$first{'name'};
 
-my ($size, $unit) = getSize($containSize);
+my ($size, $unit) = $plate->getSize($containSize);
 my $salt = Ingredient->new(
 			itemId => $keys[0],
 			name => $first{'name'},
@@ -51,34 +44,3 @@ my $salt = Ingredient->new(
 print $salt->price, "\n", $salt->size, "\n", $salt->ppu,"\n";
 
 =cut
-
-
-sub getItem {
-	my ($key, $query) = @_;
-	my $search = "http://api.walmartlabs.com/v1/search?apiKey=" . $key ."&query=". $query .
-					"&format=XML&sort=relevance&responseGroup=full";
-	my $ua = LWP::UserAgent->new();
-	my $response = $ua->get($search);
-	my $xml = XML::Simple->new();
-	if($response->is_success){
-		my $content = $xml->XMLin($response->decoded_content, keyAttr => 'itemId', forceArray => 0);
-		return $content if defined($content);
-	}
-	else {
-		print $response->status_line;
-	}
-	return 0;
-}
-
-#Sometimes there is no size field, so we have to improvise. Size seems to also show up in the name field
-sub getSize {
-	my $name = shift;
-	
-	$name =~ /(\d+\s\w+)/;
-	if(defined($1)){
-		my $size = $1;
-		my @toReturn = split(' ', $size);
-		return wantarray ? @toReturn: $toReturn[0]; 
-	}
-	return wantarray ? (0,"0") : 0;
-}
